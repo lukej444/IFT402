d90e55ef8cc04d5a17be6b9b6be05c1cf951954c
willdunhamasu <william.dunham@asu.edu>
2023-02-19 10:44:20 -0800

    updated models file.

--- roger_wilco/training_app/models.py
+++ roger_wilco/training_app/models.py
@@ -1,3 +1,40 @@
+from email.policy import default
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+	Employee_ID = models.IntegerField(primary_key=True)
+	Last_Name = models.CharField(max_length=16)
+	First_Name = models.CharField(max_length=16)
+	Street_Address = models.CharField(max_length=75)
+	City = models.CharField(max_length=16)
+	State = models.CharField(max_length=10)
+	Zip_Code = models.IntegerField(default=00000)
+	Job_Assignment = models.CharField(max_length=25)
+	Department_ID = models.ForeignKey(Department, on_delete=models.CASCADE)
+	Supervisor_ID = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
+	
+
+	def __str__(self):
+		return self.Employee_ID
+
+class Department(models.Model):
+	Department_ID = models.IntegerField(primary_key=True)
+	Department_Name = models.CharField(max_length=50)
+	Supervisor_ID = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.Department_ID
+
+class Supervisor(models.Model):
+	Employee_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
+	Department_ID = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.Employee_ID
+
+class Training(models.Model):
+	Training_ID = models.IntegerField()
+
+	Employee_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
